52fd292e9619b66d8e0a1e5c733f36c7ec98a145
eloanapontes <eloana.pontes@gmail.com>
2024-11-01 02:08:07 -0300

    retorno de info sql

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import model.Cena;
+import model.Item;
 import repository.CenaDAO;
+import repository.ItemDAO;
 
 import java.sql.SQLException;
 
@@ -7,7 +9,10 @@ public class Main {
     public static void main(String[] args) {
         try {
             Cena cena = CenaDAO.findCenaById(1);
+            Item item = ItemDAO.findItesByScene(1);
 
+
+            System.out.println(item.toString());
             System.out.println(cena.toString());
         } catch (SQLException e) {
             throw new RuntimeException(e);
--- src/repository/ItemDAO.java
+++ src/repository/ItemDAO.java
@@ -1,9 +1,32 @@
 package repository;
 
+import model.Cena;
 import model.Item;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 public class ItemDAO {
-    public static Item findItemById(int id){ //receber parametro do ID e retornar instância da classe Item quando o id for encontrado no banco
-        return new Item();
+   // public static Item findItemById(int id){ //receber parametro do ID e retornar instância da classe Item quando o id for encontrado no banco
+        //return new Item();
+  //  }
+
+    public static Item findItesByScene(int id) throws SQLException {
+        Connection conn = Mysql.getConnection();
+        String sql = "SELECT * FROM item WHERE id_item = ?";
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setInt(1,id);
+        ResultSet rs = stmt.executeQuery();
+        Item item = new Item();
+
+        if (rs.next()){
+            item.setId_item(rs.getInt("id_item"));
+            item.setDescricaoPositiva(rs.getString("descricaoPositiva"));
+
+        }return item;
     }
+
 }
