5c1c5f82e5e4de4c14857daf66e9ebaaf1e1f9bf
eloanapontes <eloana.pontes@gmail.com>
2024-10-31 22:19:39 -0300

    re4 model

---
+++ src/model/Cena.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Cena {
+    private int id_cena;
+    private String descricao;
+    private boolean status;
+
+}
---
+++ src/repository/Mysql.java
@@ -0,0 +1,28 @@
+package repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Mysql {
+    private static Connection connection;
+
+    public static Connection getConnection(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/tf3final",
+                    "root",
+                    ""
+            );
+            return connection;
+        }catch (SQLException e){
+            System.out.println("Erro ao tentar conectar com o banco de dados");
+        } catch (ClassNotFoundException e){
+            System.out.println("Erro ao tentar importar o driver do mysql");
+        }
+        return null;
+    }
+
+
+}
